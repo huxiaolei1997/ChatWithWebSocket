3df24dc81dc47f48c83df214878bc7b9c477e669
xiaolei hu <huxiaolei1997@gmail.com>
2018-06-10 16:13:13 +0800

    update

--- src/main/java/com/chat/mapper/UserMapper.java
+++ src/main/java/com/chat/mapper/UserMapper.java
@@ -3,7 +3,6 @@ package com.chat.mapper;
 import com.chat.model.Friend;
 import com.chat.model.Message;
 import com.chat.model.User;
-import com.chat.model.UserTest;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
